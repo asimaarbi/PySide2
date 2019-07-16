e3c0fa1b31d9362bbc3dbc8224979d39de22f682
asimfarooq <asim@asimfarooqs-MacBook-Pro.local>
2019-07-16 12:53:05 +0500

    PySide2- Display html in label

--- helloworld.py
+++ helloworld.py
@@ -5,5 +5,9 @@ from PySide2.QtWidgets import QApplication, QLabel
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     label = QLabel("Hello World")
+
+    # Displaying html in the label
+    label = QLabel("<font color=red size=40>Hello World</font>")
+
     label.show()
     sys.exit(app.exec_())
